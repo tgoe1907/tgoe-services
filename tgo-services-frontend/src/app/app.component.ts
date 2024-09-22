ef4f539a328c7e93720dc73414df7fefa18d807b
Adrian Furrer <adrian.furrer1999@gmail.com>
2024-09-22 18:59:56 +0200

    defined basic read and write operation with api

--- tgo-services-frontend/src/app/app.component.ts
+++ tgo-services-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tgo-services-frontend';
-  test_variable = 'Irgendein scheiß';
-  hidden = false;
-  constructor(){}
-  mystery() {
-    this.hidden = !this.hidden;
+
+  constructor(private apiService: ApiService) {}
+
+  public users: any[] = [];
+
+  public user: any = {
+    forname: '',
+    lastname: '',
+    email: '',
+    date: '',
+  };
+
+  ngOnInit(): void {
+    this.getUsers();
   }
+
+  getUsers(): void {
+    this.apiService.getUsers().subscribe(
+      (response) => {
+        this.users = response;
+        console.log('API response: ', response);
+      },
+      (error) => {
+        console.error('API error: ', error);
+      }
+    )
+  }
+
+  createUser(): void {
+    this.apiService.createUser(this.user).subscribe(
+      (response) => {
+        console.log('User erstellt', response)
+      },
+      (error) => {
+        console.error('Fehler beim errstellen', error)
+      }
+    )
+  }
+
 }
