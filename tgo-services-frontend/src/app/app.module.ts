1298c5d5a652e55a5cf5d31890ff9b2d611074f0
Adrian Furrer <adrian.furrer1999@gmail.com>
2025-01-19 19:59:15 +0100

    WIP

--- tgo-services-frontend/src/app/app.module.ts
+++ tgo-services-frontend/src/app/app.module.ts
@@ -4,22 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MonthComponent } from './calendar/month/month.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    MonthComponent
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        MonthComponent], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
